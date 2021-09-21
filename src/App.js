38e711031229e945ea4d25d8ecf2010398434e59
dev-cmkim <cm971003@gmail.com>
2021-09-21 22:43:59 +0900

    profile 화면 구성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './assests/App.css';
 import About from './components/About';
 import Main from './components/Main';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Main></Main>
       <About></About>
+      <Profile></Profile>
     </div>
   );
 }
