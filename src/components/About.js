eb47ae29d281dd5dd16e199b84f994c68f0d9a93
dev-cmkim <cm971003@gmail.com>
2021-09-21 20:52:54 +0900

    about 화면 구성

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import myImg from '../assests/images/me.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faStarOfDavid,
+    faCode,
+    faPeopleCarry,
+  } from "@fortawesome/free-solid-svg-icons";
+
+
+const About = () => {
+    return (  
+        <div className="about">
+            <h2 className="pointText">ABOUT</h2>
+            <ul className="topBox">
+              <li>
+                 <FontAwesomeIcon className="icon" icon={faStarOfDavid}  />
+                 <h3>도전</h3>
+                 <p>도전하는것이 두렵지 않습니다.</p>
+              </li>
+              <li>
+                 <FontAwesomeIcon className="icon" icon={faCode}  />
+                 <h3>끈기</h3>
+                 <p>쉽게 포기하지 않습니다.</p>
+              </li>
+              <li>
+                 <FontAwesomeIcon className="icon" icon={faPeopleCarry}  />
+                 <h3>소통</h3>
+                 <p>서로 도우며 함께 협력하겠습니다.</p>
+              </li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default About;
