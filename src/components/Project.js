84babfd98f328452634a02bdcb417cab4d9d44ba
dev-cmkim <cm971003@gmail.com>
2021-10-18 23:35:50 +0900

    slick-slider 연동 + (projects 이미지 추가)

---
+++ src/components/Project.js
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCss3,
+  faHtml5,
+  faJs,
+  faReact,
+  faJava,
+  faLinux,
+  faSwift,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Project = () => {
+  return (
+    <div className="skills">
+      <h3 className="pointText">PROJECT</h3>
+    </div>
+  );
+};
+
+export default Project;
